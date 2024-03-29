dff40fcaa85ee1659564cc9b342d78f02bfab916
EwanTrollip <ewantrollip@gmail.com>
2024-03-29 12:56:49 +0200

    update

---
+++ src/app/components/global/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="flex h-16 flex-row items-center justify-between border-t p-4">
+      <div>LOGO</div>
+      <div>menu</div>
+    </footer>
+  );
+}
