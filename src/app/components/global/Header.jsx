79bf439d6dc40e4683dbc77c628b87c05a09dd49
Ewan <108713859+M1NDLE55@users.noreply.github.com>
2024-03-29 12:56:49 +0200

    update

---
+++ src/app/components/global/Header.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="flex h-16 flex-row items-center justify-between border-b p-4">
+      <div>LOGO</div>
+      <Menu />
+    </header>
+  );
+}
+
+function Menu() {
+  const links = [
+    { href: "/", text: "Home" },
+    { href: "/quote", text: "Quote" },
+  ];
+
+  return (
+    <div className="flex flex-row gap-2">
+      {links.map((link) => (
+        <Link key={link.href} href={link.href} className="">
+          {link.text}
+        </Link>
+      ))}
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,153 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
+    <main className="px-4 pb-4">
+      <div className="absolute left-0 top-0 -z-10 h-dvh w-full bg-gradient-to-b from-rose-50 via-red-200 to-white p-0" />
+      <div className="h-section-1 center-div flex-col ">
+        <h1 className="text-balance text-center text-5xl font-bold leading-snug sm:text-6xl">
+          It&apos;s Not <span className="text-[#8338ec]">Just a Website</span>,
+          <br />
+          It&apos;s an{" "}
+          <span className="text-[#fb5607]">Unforgettable Experience</span>
+        </h1>
+        <Link
+          href="/quote"
+          className="effect-glass mt-10 flex w-fit rounded-md px-6 py-4 transition-all hover:scale-105"
+        >
+          <span className="text-2xl">Get a Quote</span>
+        </Link>
+      </div>
+      <div className="center-div">
+        <p className="text-xl sm:w-[700px] sm:text-2xl">
+          Hey there, welcome to COMPANY. So, here&apos;s the deal &ndash; not
+          having a killer website is like being invisible to potential
+          customers. You&apos;re missing out on opportunities left, right, and
+          center! But don&apos;t sweat it, because that&apos;s where I come in.
+          I specialize in solving the whole &apos;invisible&apos; problem by
+          creating websites that not only show off your brand but also hook
+          visitors and turn them into loyal fans.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="center-div mt-10 flex-col">
+        <h2 className="mb-10 w-full max-w-[700px] text-xl sm:text-2xl">
+          So, if you&apos;re ready to take your online presence to the{" "}
+          <span className="font-bold text-[#fb5607]">next level</span>,
+          here&apos;s how I can help:
+        </h2>
+        <Services />
       </div>
     </main>
+  );
+}
+
+function Services() {
+  const services = [
+    {
+      service: "Website Development",
+      description:
+        "Designing and building custom websites tailored to your needs",
+      tag: "dev",
+    },
+    {
+      service: "Domain, Email, and Hosting Management",
+      description:
+        "Registering and managing domain names, setting up professional email addresses, and handling website hosting",
+      tag: "hosting",
+    },
+    {
+      service: "SEO Optimization",
+      description:
+        "Optimizing your website to improve its visibility and ranking in search engine results",
+      tag: "seo",
+    },
+    {
+      service: "Content Creation",
+      description:
+        "Crafting engaging content for your website, including text, images, and multimedia elements",
+      tag: "content",
+    },
+    {
+      service: "Social Media Integration",
+      description:
+        "Seamlessly connecting your website with social media platforms to enhance engagement and reach",
+      tag: "social",
+    },
+    {
+      service: "E-commerce Solutions",
+      description:
+        "Setting up online stores and integrating secure payment gateways for e-commerce functionality",
+      tag: "ecom",
+    },
+    {
+      service: "Analytics and Reporting",
+      description:
+        "Implementing tools to track website performance and user behavior, and providing insightful reports",
+      tag: "report",
+    },
+    // {
+    //   service: "Security Measures",
+    //   description:
+    //     "Implementing measures to protect your website from security threats and ensuring data safety",
+    // },
+    {
+      service: "Mobile First Design",
+      description:
+        "Optimizing your website for seamless viewing and usability on mobile devices",
+      tag: "mfd",
+    },
+    // {
+    //   service: "Conversion Rate Optimization (CRO)",
+    //   description:
+    //     "Analyzing user behavior and implementing strategies to improve your website's conversion rate",
+    // },
+  ];
+
+  const colors = ["#2d00f7", "#8900f2", "#b100e8", "#d100d1", "#e500a4"];
+
+  const results = [];
+
+  services.forEach((service, i) => {
+    let index = Math.floor(Math.random() * 5);
+
+    if (i === 0) {
+      results.push({ ...service, color: colors[index] });
+      return;
+    }
+
+    while (i === 1) {
+      if (colors[index] !== results[0].color) {
+        results.push({ ...service, color: colors[index] });
+        return;
+      }
+      index = Math.floor(Math.random() * 5);
+    }
+
+    while (i > 1) {
+      if (
+        colors[index] !== results[i - 1].color &&
+        colors[index] !== results[i - 2].color
+      ) {
+        results.push({ ...service, color: colors[index] });
+        return;
+      }
+      index = Math.floor(Math.random() * 5);
+    }
+  });
+
+  return (
+    <div className="center-div grid grid-cols-1 gap-4 lg:grid-cols-2">
+      {results.map((result, i) => (
+        <Link
+          key={i}
+          href={`/quote?ref=${result.tag}`}
+          className="hover: flex w-full max-w-[700px] flex-col rounded-md border p-4 shadow lg:h-28"
+          style={{ borderColor: result.color }}
+        >
+          <h3 className="text-xl">{result.service}</h3>
+          <p>{result.description}</p>
+        </Link>
+      ))}
+    </div>
   );
 }
