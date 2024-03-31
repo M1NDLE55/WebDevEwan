72078acada5fe4974a481caa2b447f2aa5979468
EwanTrollip <ewantrollip@gmail.com>
2024-03-31 11:29:45 +0200

    fixed Link scroll "bug"

--- src/app/about-me/page.js
+++ src/app/about-me/page.js
@@ -1,16 +1,7 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-import { useEffect } from "react";
-
 export default function Page() {
-  //#region fix scroll to top (Link bug)
-  const pathname = usePathname();
-
-  useEffect(() => {
-    window.scroll(0, 0);
-  }, [pathname]);
-  //#endregion
-
-  return <main className="h-section-1 p-4">about me</main>;
+  return (
+    <main className="pb-4">
+      <div className="h-section-1"></div>
+    </main>
+  );
 }
