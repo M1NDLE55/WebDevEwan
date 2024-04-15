30bc4933172fb374b9ef31fa59910f3ae31cb9cd
EwanTrollip <ewantrollip@gmail.com>
2024-04-15 21:29:36 +0200

    fix

--- src/app/about-me/page.js
+++ src/app/about-me/page.js
@@ -8,40 +8,39 @@ export default function Page() {
             <h2>Ewan Trollip</h2>
             <h2>2024</h2>
           </div>
-          <P>
-            While studying Information Technology, I became frustrated with not
-            learning how to create a production-ready app and deliver it to
-            users. Despite dedicating considerable time to studying various
-            development topics, I had little to show for it.
-          </P>
-          <P>
-            It was during this period of frustration that I discovered web
-            development. Web development not only offered solutions to my
-            problems but did so in an incredibly accessible and dynamic manner.
-            The industry keeps on changing, offering fresh chances for
-            development and learning every single day.
-          </P>
-          <P>
-            However, amidst my excitement, I encountered numerous websites that
-            could significantly improve their user experience with minimal
-            effort. These websites lacked attention to detail, which sparked my
-            passion for meticulousness. I pride myself on paying attention to
-            the small stuff - the details that users may overlook but ultimately
-            contribute to the overall narrative told by a website.
-          </P>
-          <P>
-            I firmly believe in the importance of crafting an experience, rather
-            than just building a functional website. With this in mind, I
-            approach each project with a holistic perspective, ensuring that
-            every element, from the underlying code to the visual aesthetics,
-            contributes to a seamless and engaging user experience.
-          </P>
+          <div className="mt-2 flex flex-col gap-2 text-lg sm:text-xl">
+            <p>
+              While studying Information Technology, I became frustrated with
+              not learning how to create a production-ready app and deliver it
+              to users. Despite dedicating considerable time to studying various
+              development topics, I had little to show for it.
+            </p>
+            <p>
+              It was during this period of frustration that I discovered web
+              development. Web development not only offered solutions to my
+              problems but did so in an incredibly accessible and dynamic
+              manner. The industry keeps on changing, offering fresh chances for
+              development and learning every single day.
+            </p>
+            <p>
+              However, amidst my excitement, I encountered numerous websites
+              that could significantly improve their user experience with
+              minimal effort. These websites lacked attention to detail, which
+              sparked my passion for meticulousness. I pride myself on paying
+              attention to the small stuff - the details that users may overlook
+              but ultimately contribute to the overall narrative told by a
+              website.
+            </p>
+            <p>
+              I firmly believe in the importance of crafting an experience,
+              rather than just building a functional website. With this in mind,
+              I approach each project with a holistic perspective, ensuring that
+              every element, from the underlying code to the visual aesthetics,
+              contributes to a seamless and engaging user experience.
+            </p>
+          </div>
         </div>
       </div>
     </main>
   );
-}
-
-function P({ children }) {
-  return <p className="mt-2 text-lg sm:text-xl">{children}</p>;
 }
