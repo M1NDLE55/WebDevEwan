07c2de59d0a05c8146695130d8e7758cce6f4a4f
EwanTrollip <ewantrollip@gmail.com>
2024-04-23 13:00:58 +0200

    maintenance added

--- src/app/(default)/layout.js
+++ src/app/(default)/layout.js
@@ -1,13 +1,14 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/global/Header";
-import Footer from "./components/global/Footer";
+import Header from "../components/global/Header";
+import Footer from "../components/global/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "WebDev",
-  description: "",
+  title: "Web Developer based in South Africa | WebDevEwan",
+  description:
+    "Hey there, I'm Ewan. Ready to stop being invisible to your customers? I specialize in crafting killer websites that showcase your brand, captivate visitors, and transform them into loyal fans.",
 };
 
 export default function RootLayout({ children }) {
