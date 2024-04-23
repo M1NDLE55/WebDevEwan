24c71a440615525388f6356c64ae7c8c9fd462ed
Ewan <108713859+M1NDLE55@users.noreply.github.com>
2024-04-23 13:00:58 +0200

    maintenance added

--- src/app/(default)/page.js
+++ src/app/(default)/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import Services from "./components/home/Services";
-import Shapes from "./components/home/Shapes";
+import Services from "../components/home/Services";
+import Shapes from "../components/home/Shapes";
 
 export default function Home() {
   return (
