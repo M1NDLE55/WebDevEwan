cb317b21d808ec80f6b0447fa3437588afd19273
EwanTrollip <ewantrollip@gmail.com>
2024-04-23 13:08:04 +0200

    Update layout.js

--- src/app/(default)/layout.js
+++ src/app/(default)/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Header from "../components/global/Header";
 import Footer from "../components/global/Footer";
 
