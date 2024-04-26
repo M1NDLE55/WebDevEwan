7a5d4409b1335288c23bb42f5326e616ca812f9f
EwanTrollip <ewantrollip@gmail.com>
2024-04-26 11:21:06 +0200

    Update Contact.jsx

--- src/app/components/home/Contact.jsx
+++ src/app/components/home/Contact.jsx
@@ -4,14 +4,14 @@ export default function About() {
   return (
     <div className="center-div mt-14 flex-col px-4 sm:mt-20">
       <h1 className="text-3xl sm:text-4xl">Get in Touch</h1>
-      <div className="center-div mt-5 w-full max-w-4xl flex-col gap-4 rounded-md border border-red-400 p-4 text-center md:rounded-full md:py-10">
+      <div className="center-div mt-5 w-full max-w-4xl flex-col gap-4 rounded-md border border-violet-500 p-4 text-center md:rounded-full md:py-10">
         <p className="w-full max-w-2xl text-xl sm:text-2xl">
           Don&apos;t leave your customers waiting - reach out today for a free
           quote. Let&apos;s work together to bring your dream website to life!
         </p>
         <Link
           href="/quote"
-          className="w-fit rounded-md border border-green-400 p-4 px-6 py-4 text-center text-xl shadow-md transition-colors hover:border-orange-400 sm:text-2xl"
+          className="w-fit rounded-md border border-orange-500 p-4 px-6 py-4 text-center text-xl shadow-md transition-colors hover:border-cyan-400 sm:text-2xl"
         >
           Contact Me
         </Link>
