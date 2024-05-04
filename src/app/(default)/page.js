2eeb4b8baaa8501f40ca9d8796f959492bc1fe7f
EwanTrollip <ewantrollip@gmail.com>
2024-05-04 12:22:50 +0200

    hero init

--- src/app/(default)/page.js
+++ src/app/(default)/page.js
@@ -1,5 +1,5 @@
 import Services from "../components/home/Services";
-import Shapes from "../components/home/Shapes";
+// import Shapes from "../components/home/Shapes";
 import Hero from "../components/home/Hero";
 import About from "../components/home/About";
 import Contact from "../components/home/Contact";
@@ -7,7 +7,7 @@ import Contact from "../components/home/Contact";
 export default function Home() {
   return (
     <main>
-      <Shapes />
+      {/* <Shapes /> */}
       <Hero />
       <About />
       <Services />
--- src/app/components/home/Hero.jsx
+++ src/app/components/home/Hero.jsx
@@ -1,28 +1,26 @@
 import Link from "next/link";
-import { ArrowDown } from "lucide-react";
 
 export default function Hero() {
   return (
     <div className="h-section-1 center-div flex-col px-4">
       <h1 className="text-balance text-center text-5xl font-bold sm:text-6xl">
-        It&apos;s Not <span className="text-[#8338ec]">Just a Website</span>,
-        <br />
-        It&apos;s an{" "}
-        <span className="text-[#fb5607]">Unforgettable Experience</span>
+        Hey there! I&apos;m Ewan
       </h1>
-      <Link
-        href={{ pathname: "/quote", query: { ref: "cta-t" } }}
-        className="mt-10 w-fit rounded-md border border-cyan-400 bg-white p-4 px-6 py-4 text-center text-xl shadow-md transition-colors hover:border-green-400 sm:text-2xl"
-      >
-        Get a Quote
-      </Link>
-      <Link
-        href="/#welcome"
-        className="center-div group mt-5 flex-col text-lg sm:text-xl"
-      >
-        Read More
-        <ArrowDown className="transition-transform group-hover:translate-y-2" />
-      </Link>
+      <p className="mt-4 text-lg sm:text-xl">Software Developer | Student</p>
+      <div className="center-div mt-10 flex-col gap-4 sm:flex-row sm:gap-2">
+        <Link
+          href="/#welcome"
+          className="w-full rounded-md border border-red-500 p-4 px-6 py-4 text-center text-xl shadow-lg shadow-red-200 transition-colors hover:border-red-600 sm:w-fit sm:text-2xl"
+        >
+          Read More
+        </Link>
+        <Link
+          href={{ pathname: "/quote", query: { ref: "cta-t" } }}
+          className="w-fit rounded-md bg-red-500 p-4 px-6 py-4 text-center text-xl text-white shadow-lg transition-colors hover:bg-red-600 sm:text-2xl"
+        >
+          Contact Me
+        </Link>
+      </div>
     </div>
   );
 }
