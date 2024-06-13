eb8ade483a89c63b39b41e9f9b6d00d767b03da5
EwanTrollip <ewantrollip@gmail.com>
2024-06-13 11:08:58 +0200

    Restart - Hero

--- src/app/components/home/About.jsx
+++ src/app/components/home/About.jsx
@@ -1,11 +1,9 @@
 export default function About() {
   return (
-    <div id="about" className="center-div flex-col px-4">
-      <h2 className="max-w-2xl text-balance text-center text-3xl sm:text-5xl">
-        About Me
-      </h2>
-      <div className="center-div mt-5 w-full max-w-4xl rounded-2xl px-4 md:border md:border-red-500 md:bg-white md:py-10 md:shadow-lg md:shadow-red-200">
-        <p className="w-full max-w-2xl text-center text-xl sm:text-2xl">
+    <div id="about" className="center-div bg-white pt-10">
+      <div className="w-full max-w-3xl px-4">
+        <h2 className="text-3xl sm:text-5xl">About Me</h2>
+        <p className="mt-4 text-xl sm:text-2xl">
           Hey there, I&apos;m Ewan . So, here&apos;s the deal &ndash; not having
           a killer website is like being invisible to potential customers.
           You&apos;re missing out on opportunities left, right, and center! But
