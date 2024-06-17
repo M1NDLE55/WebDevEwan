325f33d58d85b078b3979e7d82d4b3b472e9f291
Ewan <108713859+M1NDLE55@users.noreply.github.com>
2024-06-17 12:14:53 +0200

    hero-about divider

--- src/app/components/home/About.jsx
+++ src/app/components/home/About.jsx
@@ -1,18 +1,32 @@
 export default function About() {
   return (
-    <div id="about" className="center-div bg-white pt-10">
-      <div className="w-full max-w-3xl px-4">
-        <h2 className="text-3xl sm:text-5xl">About Me</h2>
-        <p className="mt-4 text-xl sm:text-2xl">
-          Hey there, I&apos;m Ewan . So, here&apos;s the deal &ndash; not having
-          a killer website is like being invisible to potential customers.
-          You&apos;re missing out on opportunities left, right, and center! But
-          don&apos;t sweat it, because that&apos;s where I come in. I specialize
-          in solving the whole &apos;invisible&apos; problem by creating
-          websites that not only show off your brand but also hook visitors and
-          turn them into loyal fans.
-        </p>
+    <>
+      <svg
+        className="h-16 w-full"
+        preserveAspectRatio="none"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="-720.1 -76.144 720.2 76.24"
+      >
+        <path
+          d="M-63.2985-69.37C-49.9434-63.68-38.324-54.582-29.6081-42.9899L0 0-720 0C-720 0-709.99-12.9202-690.075-16.4792L-105.5-75.5C-91.2745-77.1415-76.654-75.06-63.2985-69.37Z"
+          fill="white"
+        />
+      </svg>
+      <div id="about" className="center-div bg-white pt-12">
+        <div className="w-full max-w-3xl px-4">
+          <h2 className="text-3xl sm:text-5xl">About Me</h2>
+          <p className="mt-4 text-xl sm:text-2xl">
+            Hey there, I&apos;m Ewan . So, here&apos;s the deal &ndash; not
+            having a killer website is like being invisible to potential
+            customers. You&apos;re missing out on opportunities left, right, and
+            center! But don&apos;t sweat it, because that&apos;s where I come
+            in. I specialize in solving the whole &apos;invisible&apos; problem
+            by creating websites that not only show off your brand but also hook
+            visitors and turn them into loyal fans.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
