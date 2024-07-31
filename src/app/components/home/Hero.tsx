1f9cb62e0af40ca85ff0ada7695c3b3f75b93a53
EwanTrollip <ewantrollip@gmail.com>
2024-07-31 15:39:55 +0200

    updated about

--- src/app/components/home/Hero.tsx
+++ src/app/components/home/Hero.tsx
@@ -3,7 +3,7 @@ import { Spotlight } from "../ui/Spotlight";
 
 export default function Hero() {
   return (
-    <div className="bg-grid-violet-100/[0.03] relative flex h-screen w-full overflow-hidden rounded-md bg-black/[0.96] antialiased md:items-center md:justify-center">
+    <div className="relative flex h-screen w-full overflow-hidden rounded-md bg-black/[0.96] antialiased bg-grid-violet-300/[0.03] md:items-center md:justify-center">
       <Spotlight
         className="-top-40 left-0 md:-top-20 md:left-60"
         fill="white"
@@ -18,7 +18,7 @@ export default function Hero() {
         </p>
         <Link
           href=""
-          className="animate-shimmer mt-6 inline-flex h-12 items-center justify-center rounded-md border border-neutral-50 bg-[linear-gradient(110deg,#000103,45%,#4c1d95,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-neutral-50 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+          className="mt-6 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-neutral-50 bg-[linear-gradient(110deg,#000103,45%,#4c1d95,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-neutral-50 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-400 focus:ring-offset-2 focus:ring-offset-violet-50"
         >
           View my projects
         </Link>
